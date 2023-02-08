8f9c0df7297468b4094aae8670392cef69faa0d7
romankt21 <romankt21@gmail.com>
2023-02-08 22:44:56 +0200

    change

---
+++ js_dec_22_les1/js_dec_22_lect1/lect1_main_js_dec_22.js
@@ -0,0 +1,39 @@
+// console.log(0.1 + 0.2);
+
+// let big = BigInt(123456);
+// console.log(big)
+//
+//
+// console.log('hello' === 'hello');
+// console.log('hel' < 'hello');
+
+// console.log(parseInt('12wert'));
+//
+// console.log(parseFloat(12.45));
+
+// console.log(!!10);
+// console.log(!!-10);
+// console.log(!!'qw');
+// console.log(!![]);
+// console.log(!!{});
+//
+//
+// console.log(!!0);
+// console.log(!!'');
+// console.log(!!NaN);
+// console.log(!!null);
+// console.log(!!undefined);
+
+let undef = undefined;
+console.log(undef);
+console.log(typeof undef);
+
+
+let nan = NaN;
+console.log(nan);
+console.log(typeof nan);
+
+let nul = null;
+console.log(null);
+console.log(typeof nul);
+
