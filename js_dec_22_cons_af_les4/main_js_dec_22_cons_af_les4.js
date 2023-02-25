00fd386827eae2de97379ec818499789c2183c42
romankt21 <romankt21@gmail.com>
2023-02-25 14:02:53 +0200

    change

--- js_dec_22_cons_af_les4/main_js_dec_22_cons_af_les4.js
+++ js_dec_22_cons_af_les4/main_js_dec_22_cons_af_les4.js
@@ -1,10 +1,15 @@
-
+// //- створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
 // function rectangle (a, b) {
 //
 //     return a * b
 // }
 //
 // console.log(rectangle(5, 6));
+
+
+//  //  створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий.
+//  Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
+
 
 // function fun (text, numb) {
 //     for (const textElement of text) {
@@ -18,31 +23,49 @@
 // fun('tre', 3)
 
 
-function fun1 (text1, numb1) {
-    for (let i = 0; i < numb1; i++) {
+// function fun1 (text1, numb1) {
+//     for (let i = 0; i < numb1; i++) {
+//
+//         document.write(`<ul>`)
+//         document.write(`<li> ${text1} </li>`)
+//         document.write(`</ul>`)
+//     }
+//
+// }
+//
+// fun1('tre', 3)
+//
+//
+// function fun2 (text2, numb2) {
+//
+//     document.write(`<ul>`)
+//     for (let i = 0; i < numb2; i++) {
+//
+//
+//         document.write(`<li> ${text2} </li>`)
+//
+//     }
+//     document.write(`</ul>`)
+// }
+//
+// fun2('tre', 3)
 
-        document.write(`<ul>`)
-        document.write(`<li> ${text1} </li>`)
-        document.write(`</ul>`)
-    }
 
-}
+//  // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
+let array = ['str', 1, 55, true, [1, 5, -25]]
 
-fun1('tre', 3)
-
-
-function fun2 (text2, numb2) {
-
+function fun (arr) {
     document.write(`<ul>`)
-    for (let i = 0; i < numb2; i++) {
 
-
-        document.write(`<li> ${text2} </li>`)
-
+    for (const item of arr) {
+        document.write(`<li> ${item} </li>`)
     }
     document.write(`</ul>`)
 }
 
-fun2('tre', 3)
+fun(array)
 
-10/33
+
+
+
+// 18.50
