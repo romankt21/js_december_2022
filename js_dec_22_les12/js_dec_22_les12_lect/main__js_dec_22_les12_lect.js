42f77feb708995aaee4d62db30f8381d674f1dd9
romankt21 <romankt21@gmail.com>
2023-03-14 22:05:57 +0200

    change

---
+++ js_dec_22_les12/js_dec_22_les12_lect/main__js_dec_22_les12_lect.js
@@ -0,0 +1,16 @@
+
+
+// let url = new URL("http://localhost:63342/js_december_2022/js_dec_22_les12/js_dec_22_les12_lect/index_js_dec_22_les12_lect.html?_ijt=dtn49kul5bq65qgjb721scp239&_ij_reload=RELOAD_ON_SAVE");
+// console.log(url);
+//
+// console.log(url.searchParams)
+//
+// url.searchParams.set('xxx', 888)
+// console.log(url);
+//
+// let  xxxParamValue = url.searchParams.get('xxx')
+// console.log(xxxParamValue);
+
+
+// 14.00
+//let url = new UR
