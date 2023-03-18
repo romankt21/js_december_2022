17e74b447f63414357d641a4dd3da9db1211467f
romankt21 <romankt21@gmail.com>
2023-03-18 14:58:03 +0200

    change

--- mini_proekt_js_dec_22/main.js
+++ mini_proekt_js_dec_22/main.js
@@ -4,15 +4,15 @@ document.body.appendChild(usersArr);
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(value => value.json())
     .then(users => users.forEach(user => {
-        let userItem = document.createElement('div')
-        userItem.classList.add('user');
+        let userDiv = document.createElement('div')
+        userDiv.classList.add('user');
         let a = document.createElement('a');
         a.innerText = `${user.id} ${user.name}`;
         a.href = `./user-details.html?data=${JSON.stringify(user)}`;
 
 
-        usersArr.appendChild(userItem)
-            userItem.appendChild(a)
+        usersArr.appendChild(userDiv)
+            userDiv.appendChild(a)
 
 
     }))
--- mini_proekt_js_dec_22/user-details.js
+++ mini_proekt_js_dec_22/user-details.js
@@ -1,15 +1,45 @@
 let url = new URL(location.href);
-let json = url.searchParams.get('data');
-let user = JSON.parse(json);
+let jsonUser = url.searchParams.get('data');
+let user = JSON.parse(jsonUser);
+console.log(jsonUser);
 console.log(user);
+
+for (const userAdr of user.adress) {
+    console.log(userAdr)
+
+}
 
 let divUserDetails = document.createElement('div');
 document.body.appendChild(divUserDetails);
-divUserDetails.innerText = `${json}`
+divUserDetails.innerText =`id: ${user.id}, name: ${user.name}, username: ${user.username}, email: ${user.email} , address: ${(user.address.forEach)}`
 
-let button = document.createElement('button');
-button.innerText = 'posts';
 
-button.onclick = () => `{location.href =./post-details.html?data=${JSON.stringify(postItem)}}`;
-divUserDetails.appendChild(button)
+let p = document.createElement('div')
+document.body.appendChild(p);
+p.innerText = '----------'
 
+let dv = document.createElement('div');
+document.body.appendChild(dv);
+dv.innerText =`${jsonUser}`
+
+
+//
+// let postsButton = document.createElement('button');
+// postsButton.innerText = 'post of current user';
+//
+// postsButton.classList.add('postsButton');
+//
+//
+// postsButton.onclick = () => `{location.href =./post-details.html?data=${JSON.stringify(postItem)}}`;
+// divUserDetails.appendChild(postsButton)
+
+
+
+
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//     .then(value => value.json())
+//     .then(post => posts.forEach(post => {
+//         if (post.id === user.id) {
+//
+//         }
+//     }))
